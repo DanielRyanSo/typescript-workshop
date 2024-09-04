712f2ba3e90be135aad568b799ae7e9a75c9f450
DanielRyanSo <23101898@usc.edu.ph>
2024-09-04 14:42:25 +0800

    commit

--- build/src/index.js
+++ build/src/index.js
@@ -1,26 +1,38 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', {value: true});
 exports.doSomeStuff = doSomeStuff;
 console.log('Try npm run lint/fix!');
-const longString = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+const longString =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
 const trailing = 'Semicolon';
-const why = { am: 'I tabbed?' };
+const why = {am: 'I tabbed?'};
 const iWish = "I didn't have a trailing space...";
 const sicilian = true;
 const vizzini = sicilian ? !sicilian : sicilian;
-const re = /foo {3}bar/;
+const re = /foo\s{3}bar/; // Adjust based on actual spacing intention
 function doSomeStuff(withThis, andThat, andThose) {
-    //function on one line
-    if (!andThose.length) {
-        return false;
-    }
-    console.log(withThis);
-    console.log(andThat);
-    console.dir(andThose);
-    console.log(longString, trailing, why, iWish, vizzini, re);
-    return;
+  if (andThose.length === 0) {
+    return false;
+  }
+  console.log(withThis);
+  console.log(andThat);
+  console.dir(andThose);
+  console.log(longString, trailing, why, iWish, vizzini, re);
+  return;
 }
-// TODO: more examples
-const numbers = [2, 4, 6, 8];
-console.log(sqaureNumber(numbers));
+const filter_1 = require('./filter');
+const map_1 = require('./map');
+const slice_1 = require('./slice');
+const foreach_1 = require('./foreach');
+const words = ['Berry', 'Banana', 'Mango', 'Dragon Fruit'];
+const numbers = [1, 2, 3, 4, 5];
+const myString = 'JavaScript Workshop Activity in WebDev 2';
+const fruits = ['Berry', 'Banana', 'Mango', 'Dragon Fruit'];
+const filteredLongWords = (0, filter_1.filterLongWords)(words);
+console.log('Filtered Long Words:', filteredLongWords);
+const thriceTheNumbers = (0, map_1.thriceTheNum)(numbers);
+console.log('Thrice The Numbers:', thriceTheNumbers);
+const slicedChar = (0, slice_1.getSlicedString)(myString);
+console.log('Sliced Characters:', slicedChar);
+(0, foreach_1.logFruits)(fruits);
 //# sourceMappingURL=index.js.map
